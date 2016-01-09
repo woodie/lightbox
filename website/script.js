656d924e2a645d296c345ab2e59930a12e3d8de6
John Woodell <woodie@netpress.com>
2016-01-09 01:21:25 -0800

    Clean up nav, add thumbnail paging

--- website/script.js
+++ website/script.js
@@ -1,16 +1,13 @@
-var flickr_user_id = '46910958@N02';
-var flickr_api_key = '10e3bd8029bd2e9ef8b1447352535c67';
-var flickr_method  = 'flickr.people.getPublicPhotos';
 var photo_data = []; // from flickr API
-var page_index = 0;
+var page_index = 0;  // selected photo
 
 function request_photos() {
   // We could also show error_mode on timeout
-  var args = {'method': flickr_method,
-             'api_key': flickr_api_key,
-             'user_id': flickr_user_id,
-        'jsoncallback': 'load_photos',
-              'format': 'json'};
+  var args = {'jsoncallback': 'load_photos',
+      'format': 'json',
+      'method': 'flickr.people.getPublicPhotos',
+      'api_key': '10e3bd8029bd2e9ef8b1447352535c67',
+      'user_id': '46910958@N02'};
   var script = document.createElement('script');
   script.src = 'https://api.flickr.com/services/rest/?' +
       Object.keys(args).map(function (k) {return `${k}=${args[k]}`}).join('&');
@@ -20,30 +17,48 @@ function request_photos() {
 function load_photos(api_data) {
   try {
     photo_data = api_data['photos']['photo'];
-    show_page(0);
+    build_thumbs();
+    render_view(0);
   } catch (e) {
     console.log(e);
     error_mode('Oops, please try back later.');
   }
 }
 
-function show_page(offset) {
-  var size = 'b'; // [mstzb]
-  var index = page_index + offset;
-  page_index = index;
-  photo = photo_data[index];
-  var img_url = "https://farm" + photo['farm'] + ".staticflickr.com/" +
-      photo['server'] + "/" + photo['id'] + "_" + photo['secret'] + "_" + size + ".jpg";
-  document.getElementById('photo_image').style.backgroundImage = "url('" + img_url + "')";
-  document.getElementById('image_title').innerHTML = photo['title'];
-  document.getElementById('left_arrow').style.visibility = (index === 0) ? "hidden" : "visible";
-  document.getElementById('right_arrow').style.visibility = (index < photo_data.length - 1) ? "visible" : "hidden";
+function img_url(photo, size) {
+  var size = size || 'b'; // [mstzb]
+  return "https://farm" + photo['farm'] + ".staticflickr.com/" + photo['server'] +
+      "/" + photo['id'] + "_" + photo['secret'] + "_" + size + ".jpg";
+}
+
+function build_thumbs() {
+  var nav_html = '';
+  for (var t = 0; t < photo_data.length; t++) {
+    nav_html += `<img id='thumb_${t}' class='thumbnail ${t === 0 ? "selected" : ''}' ` +
+        `onclick='render_view(${t})' src="${img_url(photo_data[t], 't')}">`;
+  }
+  document.getElementById('thumbnails').innerHTML = nav_html;
+}
+
+function nav_btn(next) {
+  render_view(page_index + (next ? 1 : -1));
+}
+
+function render_view(page) {
+  page_index = page;
+  document.getElementById('photo_image').style.backgroundImage = "url('" + img_url(photo_data[page]) + "')";
+  document.getElementById('photo_title').innerHTML = photo_data[page]['title'];
+  document.getElementById('left_arrow').style.visibility = (page === 0) ? "hidden" : "visible";
+  document.getElementById('right_arrow').style.visibility = (page < photo_data.length - 1) ? "visible" : "hidden";
+  var thumbs = document.getElementsByClassName("thumbnail");
+  for (var i = 0; i < thumbs.length; i++) {
+    thumbs[i].className = (thumbs[i].id === "thumb_" + page) ? 'thumbnail selected' : 'thumbnail';
+  }
 }
 
 function error_mode(message) {
   var img_url = 'https://gretchenrubin.com/wp-content/uploads/2013/02/broken-window.jpg';
   document.getElementById('photo_image').style.backgroundImage = "url('" + img_url + "')";
-  document.getElementById('image_title').innerHTML = message;
-  document.getElementById('mouse_type').style.visibility = "hidden";
-
+  document.getElementById('photo_title').innerHTML = message;
+  document.getElementById('copyright').style.visibility = "hidden";
 }
